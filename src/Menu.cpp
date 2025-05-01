59ab0f769c40ac2d4965894641ceaaa9c3767309
Gabriel Silveira <56368014+Silvxo@users.noreply.github.com>
2025-05-01 12:18:46 -0300

    add Cena creditos e opcao no menu

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -7,5 +7,11 @@ Menu::~Menu(){}
 int Menu::run(){
     arquivos.exibirCena("menu");
     int input = userInput.rangedReadNumber(1,3);
+    if(input == 3){
+        arquivos.exibirCena("creditos");
+        userInput.rangedReadNumber(1,1);
+        return run();
+
+    }
     return input;
 }
